f18c1197cff4f1806c319d400f3a76365869528e
huanc0de <arturfurtak5@gmail.com>
2023-12-19 22:37:17 +0100

    added photos for feed

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,10 +1,22 @@
 import styles from './Post.module.scss';
+
+// temp cuz we dont have backend
+import photo from '../../photos/1.webp';
+import avatar from '../../photos/avatar.webp';
+
 const Post = () => {
   return (
     <div>
       <div className={styles.post}>
-        <div className={styles.header}>name and title</div>
-        <div className={styles.photo}>photo</div>
+        <div className={styles.header}>
+          <img src={avatar} alt="random person form tpdne" />
+        </div>
+        <div className={styles.photo}>
+          <img
+            src={photo}
+            alt="bardzo przepraszam pana z tego samochodu ale akurat to bylo ostatnie w mojej galerii"
+          />
+        </div>
         <div className={styles.bottom}>
           <i>like</i>
           <i>comment</i>
--- client/src/pages/Feed/Feed.jsx
+++ client/src/pages/Feed/Feed.jsx
@@ -4,7 +4,13 @@ import styles from './Feed.module.scss';
 const Feed = () => {
   return (
     <div className={styles.container}>
-      <Post />
+      <h1 className={styles.welcome}>most recent posts</h1>
+      <div className={styles.wrapper}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </div>
   );
 };
