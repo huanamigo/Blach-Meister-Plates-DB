f0feef5a34682d73aeb5f2ab5ac505326284c5c7
huanc0de <arturfurtak5@gmail.com>
2024-01-07 22:11:34 +0100

    added links styling

--- client/src/components/Navigation/NavigationLink/NavigationLink.jsx
+++ client/src/components/Navigation/NavigationLink/NavigationLink.jsx
@@ -1,14 +1,17 @@
 import { NavLink } from 'react-router-dom';
 import styles from './NavigationLink.module.scss';
+import { Fragment } from 'react';
 
 const NavigationLink = ({ name, urlName }) => {
   return (
-    <NavLink
-      className={({ isActive }) => (isActive ? styles.active : styles.link)}
-      to={urlName}
-    >
-      {name}
-    </NavLink>
+    <Fragment className={styles.wrapper}>
+      <NavLink
+        className={({ isActive }) => (isActive ? styles.active : styles.link)}
+        to={urlName}
+      >
+        {name}
+      </NavLink>
+    </Fragment>
   );
 };
 
