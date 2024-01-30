4634d07766e3a629e4de0282c7c9f45b966f7047
huanc0de <arturfurtak5@gmail.com>
2024-01-30 21:33:11 +0100

    added slide in animation

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -9,6 +9,7 @@ import {
 import { useState } from 'react';
 import PlatesLink from '../../pages/Plates/PlatesLink/PlatesLink';
 import photos from '../Photos/Photos';
+import { useInView } from 'react-intersection-observer';
 
 const Post = ({
   author,
@@ -21,63 +22,62 @@ const Post = ({
 }) => {
   const [liked, setLiked] = useState(false);
   const [localLikes, setLocalLikes] = useState(noLikes);
+  const [ref, inView] = useInView({ threshold: 0, triggerOnce: true });
 
   return (
-    <div>
-      <div className={styles.post}>
-        <div className={styles.header}>
-          <img
-            src={photos.avatars[photoId - 1]}
-            alt="random person form tpdne"
-          />
-          <p>
-            {isDetailed ? (
-              <span className={styles.username}>{author}</span>
+    <div
+      ref={ref}
+      className={`${styles.post} ${inView ? `${styles.animation1}` : null}`}
+    >
+      <div className={styles.header}>
+        <img src={photos.avatars[photoId - 1]} alt="random person form tpdne" />
+        <p>
+          {isDetailed ? (
+            <span className={styles.username}>{author}</span>
+          ) : (
+            <PlatesLink name={author} urlName={`user/${author}`} />
+          )}
+        </p>
+      </div>
+      <div className={styles.photo}>
+        <img
+          src={photos.photos[photoId - 1]}
+          alt="bardzo przepraszam pana z tego samochodu ale akurat to bylo ostatnie w mojej galerii"
+        />
+      </div>
+      <div className={styles.bottom}>
+        <div className={styles.wrapper}>
+          <div className={styles.share}>
+            {liked ? (
+              <FaHeart
+                onClick={() => {
+                  setLiked(!liked);
+                  setLocalLikes(localLikes - 1);
+                }}
+              />
             ) : (
-              <PlatesLink name={author} urlName={`user/${author}`} />
+              <FaRegHeart
+                onClick={() => {
+                  setLiked(!liked);
+                  setLocalLikes(localLikes + 1);
+                }}
+              />
             )}
-          </p>
-        </div>
-        <div className={styles.photo}>
-          <img
-            src={photos.photos[photoId - 1]}
-            alt="bardzo przepraszam pana z tego samochodu ale akurat to bylo ostatnie w mojej galerii"
-          />
-        </div>
-        <div className={styles.bottom}>
-          <div className={styles.wrapper}>
-            <div className={styles.share}>
-              {liked ? (
-                <FaHeart
-                  onClick={() => {
-                    setLiked(!liked);
-                    setLocalLikes(localLikes - 1);
-                  }}
-                />
-              ) : (
-                <FaRegHeart
-                  onClick={() => {
-                    setLiked(!liked);
-                    setLocalLikes(localLikes + 1);
-                  }}
-                />
-              )}
-              <FaRegCommentAlt />
-              <FaRegShareSquare />
-            </div>
-            <div className={styles.category}>
-              {isDetailed ? (
-                <></>
-              ) : (
-                <PlatesLink name={name} urlName={`plates/${plate}`} />
-              )}
-            </div>
+            <FaRegCommentAlt />
+            <FaRegShareSquare />
           </div>
-          <p className={styles.likedBy}>
-            <span>{localLikes} likes</span>
-          </p>
-          <p className={styles.time}>{moment(createdTime).fromNow()}</p>
+          <div className={styles.category}>
+            {isDetailed ? (
+              <></>
+            ) : (
+              <PlatesLink name={name} urlName={`plates/${plate}`} />
+            )}
+          </div>
         </div>
+        <p className={styles.likedBy}>
+          <span>{localLikes} likes</span>
+        </p>
+        <p className={styles.time}>{moment(createdTime).fromNow()}</p>
       </div>
     </div>
   );
